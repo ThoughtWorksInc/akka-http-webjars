c7740e7ae54c447d118167c04d234ea32bcdf20f
杨博 (Yang Bo) <atryyang@thoughtworks.com>
2019-04-03 10:58:05 +0800

    Add a `sbtWebLayout` method to serve webjars in sbt-web layout

--- src/main/scala/com/thoughtworks/akka/http/WebJarsSupport.scala
+++ src/main/scala/com/thoughtworks/akka/http/WebJarsSupport.scala
@@ -1,17 +1,39 @@
-package com.thoughtworks.akka.http
+package com.thoughtworks.modularizer.server
 
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import org.webjars.WebJarAssetLocator
 
 import scala.util.{Failure, Success, Try}
 
 /**
-  * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
+  * @author 杨博 (Yang Bo)
   */
 trait WebJarsSupport {
   val webJarAssetLocator = new WebJarAssetLocator
 
-  final def webJars = {
+  final def sbtWebLayout: Route = {
+    pathPrefix("lib") {
+      pathPrefix(Segment) {
+        webJars
+      }
+    } ~ webJars
+  }
+
+  final def webJars(webJarName: String): Route = {
+    extractUnmatchedPath { path =>
+      Try(webJarAssetLocator.getFullPath(webJarName, path.toString)) match {
+        case Success(fullPath) =>
+          getFromResource(fullPath)
+        case Failure(_: IllegalArgumentException) =>
+          reject
+        case Failure(e) =>
+          failWith(e)
+      }
+    }
+  }
+
+  final def webJars: Route = {
     extractUnmatchedPath { path =>
       Try(webJarAssetLocator.getFullPath(path.toString)) match {
         case Success(fullPath) =>
